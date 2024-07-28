ea9c6875c389893f2f45608c22a24170db8a103a
vinicius <=>
2024-07-27 23:11:42 -0300

    TubeNow

--- ytsave.py
+++ ytsave.py
@@ -1,9 +1,9 @@
-from api import app, routes
+from app import app, routes
 import os
 
-if __name__ == 'main':
-    port = int(os.getenv('PORT'), '5000')
-    app.run(host='0.0.0.0', port = port)
-
 # if __name__ == 'main':
-#     app.run(debug=True)
+#     port = int(os.getenv('PORT'), '5000')
+#     app.run(host='0.0.0.0', port = port)
+
+if __name__ == 'main':
+    app.run(debug=True)
